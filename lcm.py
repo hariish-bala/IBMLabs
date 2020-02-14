30a892dd7a1ff765dcfaf3891c25f1ae19bec439
hariish-bala <60870398+hariish-bala@users.noreply.github.com>
2020-02-14 14:49:27 +0530

    Add files via upload

---
+++ lcm.py
@@ -0,0 +1,15 @@
+def compute_lcm(x, y):
+
+   if x > y:
+       greater = x
+   else:
+       greater = y
+   while True:
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+   return lcm
+num1 = int(input("enter the first valu:"))
+num2 = int(input("enter the second value:"))
+print("lcm is", compute_lcm(num1, num2))
